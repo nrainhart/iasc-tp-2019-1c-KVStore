bc7784c92670adfd1ca2b399bf21d11874f433e2
Nico <nmrainhart@gmail.com>
2019-07-06 10:33:26 -0300

    Agrego validación de nodo maestro

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-const args = require('minimist')(process.argv.slice(2))
+const express = require('express');
+const bodyParser = require('body-parser');
+const args = require('minimist')(process.argv.slice(2));
 
-var app = express();
+const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-var map = new Map();
+let soyMaster = true;
+const map = new Map();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/api/saludar', function(req, res) {
+app.get('/api/saludar', function (req, res) {
   res.send('hello world');
 });
 
-app.get('/api/conseguir', function(req, res) {
-    var data = map.get(req.query.key);
-    res.send(data);
-  });
-
-app.post('/api/insertar', function(req, res) {
-    map.set(req.body.key, req.body.value);
-    res.send("OK");
+app.get('/api/conseguir', function (req, res) {
+  validarQueSoyMaster();
+  var data = map.get(req.query.key);
+  res.send(data);
 });
+
+app.post('/api/insertar', function (req, res) {
+  validarQueSoyMaster();
+  map.set(req.body.key, req.body.value);
+  res.send('OK');
+});
+
+function validarQueSoyMaster() {
+  if(!soyMaster) {
+    throw Error('No soy el nodo maestro');
+  }
+}
+
+let masterErrorHandler = function(err, req, res, next) {
+  res.status(400).json({ error: err.toString() });
+};
+app.use(masterErrorHandler);
 
 app.listen(args['port'], function () {
   console.log('App listening on port: ' + args['port']);

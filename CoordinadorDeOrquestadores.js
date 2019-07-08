6b5e96c38cc4335758e510ca67af9f9f5bd1b510
Nico <nmrainhart@gmail.com>
2019-07-08 11:17:45 -0300

    Agrega TTL al registro de orquestador maestro en Mongo
    Para que si un orquestador se cae, eventualmente se elimine su registro de la tabla de "nodo maestro", y otro orquestador pueda registrarse como maestro

--- CoordinadorDeOrquestadores.js
+++ CoordinadorDeOrquestadores.js
@@ -2,6 +2,9 @@ const { MongoClient } = require('mongodb');
 const mongoUrl = 'mongodb://localhost:27017';
 const mongoClient = new MongoClient(mongoUrl, { useNewUrlParser: true });
 
+const MASTER_NODE_QUERY_INTERVAL_IN_SECONDS = 60;
+const MASTER_NODE_EXPIRATION_TIME_IN_SECONDS = MASTER_NODE_QUERY_INTERVAL_IN_SECONDS * 5;
+
 class CoordinadorDeOrquestadores {
 
   constructor(keyDelOrquestador) {
@@ -16,11 +19,11 @@ class CoordinadorDeOrquestadores {
   }
 
   initializeMaster() {
-    const findOrCreate = (collection, key, value) => {
+    const findOrCreate = (collection, objectToCreate) => {
       return collection.findOneAndUpdate(
         { _id: 'master node id' },
         {
-          $setOnInsert: { key: value } // updates should only happen when inserting documents
+          $setOnInsert: objectToCreate // updates should only happen when inserting documents
         },
         {
           returnOriginal: false, // return new doc if one is upserted
@@ -32,8 +35,17 @@ class CoordinadorDeOrquestadores {
       .then(client => {
         const db = client.db('kvstore');
         let masterNodeCollection = db.collection('masterNode');
-        return findOrCreate(masterNodeCollection, 'currentMasterNode', this._keyDelOrquestador)
-          .then(({ value: { key } }) => {
+        masterNodeCollection.createIndex(
+          { 'lastUpdate': 1 },
+          { expireAfterSeconds: MASTER_NODE_EXPIRATION_TIME_IN_SECONDS }
+        );
+        const masterClaim = {
+          currentMasterNode: this._keyDelOrquestador,
+          lastUpdate: new Date()
+        };
+        return findOrCreate(masterNodeCollection, masterClaim)
+          .then(({ value: lastInsertedObject }) => {
+            const key = lastInsertedObject.currentMasterNode;
             console.log(key);
             this._soyMaster = key === this._keyDelOrquestador;
           })

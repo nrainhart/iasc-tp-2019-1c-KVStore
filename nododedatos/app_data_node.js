102c88491bbc51c56e609da4acf9821cb06eb60f
Facundo Aita <facundo91@gmail.com>
2019-07-15 23:01:09 -0300

    obtener valores filtrados de un nodo por condicion

--- nododedatos/app_data_node.js
+++ nododedatos/app_data_node.js
@@ -4,7 +4,6 @@ const args = require('minimist')(process.argv.slice(2))
 
 var paresClaveValor = new Map();
 
-
 const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -59,9 +58,13 @@ app.get('/nodoDatos/obtenerValor', function(req, res) {
 app.get('/nodoDatos/obtenerMapa', function(req, res) {
   const cond = req.query.condition;
   const value = req.query.value;
-  const mapa = paresClaveValor.filter(par => this.estaCorrecto(par, cond, valor));
-  res.send(mapa);
-};
+  const map1 = new Array();
+  [...paresClaveValor]
+  .filter(([k, v]) => estaCorrecto(v.value, cond, value ))
+  .map(([k, v]) => { map1.push(v) });
+  console.log(map1.length);
+  res.send(map1);
+});
 
 app.post('/nodoDatos/guardar', function(req, res) {
   const key = req.body.key;
@@ -80,16 +83,17 @@ app.post('/nodoDatos/guardar', function(req, res) {
   }
 });
 
-app.listen(args['port'], function () {
+app.listen(8050, function () {
   console.log('App listening on port: ' + args['port']);
   app.maxSize = args['maxSize'] ? args['maxSize'] : 5;
   console.log('Key/Value max Size: ' + app.maxSize);
 });
 
-function estaCorrecto(par, cond, valor) {
+function estaCorrecto(valorClave, cond, valorCondicion) {
  if(cond == 'Mayor'){
-    return par.get(valorConTimestamp).value > valor
-  } else {
-    return  par.get(valorConTimestamp).value < valor
+    return valorClave > valorCondicion
+  } else if(cond == 'Menor') {
+    return  valorClave < valorCondicion
   }
+  return false;
 }
